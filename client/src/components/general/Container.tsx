d21f29838d181cacff3331e116b83d06d6e5801d
Satish Kumar <satishvaishna@gmail.com>
2025-07-03 20:20:56 +0530

    reset project

--- client/src/components/general/Container.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function Container({children}: any) {
-  return <div className='w-full max-w-7xl mx-auto px-4'>{children}</div>;
-}
-
-export default Container
--- client/src/types/ExpressionScores.ts
+++
@@ -1,9 +0,0 @@
-export type ExpressionScores = {
-  neutral: number;
-  happy: number;
-  sad: number;
-  angry: number;
-  fearful: number;
-  disgusted: number;
-  surprised: number;
-};
