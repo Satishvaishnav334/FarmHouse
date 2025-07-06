83e20e2283d5a0c4ab06ff2bf88a28db8076168a
Satish Kumar <satishvaishna@gmail.com>
2025-07-06 16:10:56 +0530

    Revert ".."
    This reverts commit 64cc62e5dbc17512e22d5278638e2b491194dc19.

--- Sawariya-Novelty/src/app/checkout/page.tsx
+++ Sawariya-Novelty/src/app/checkout/page.tsx
@@ -86,7 +86,6 @@ export default function Checkout() {
   const finalAmount = cart.totalAmount + deliveryFee;
 
   const handleInputChange = (field: string, value: string) => {
-    console.log("first")
     if (field.startsWith('address.')) {
       const addressField = field.split('.')[1];
       setCustomerInfo(prev => ({
